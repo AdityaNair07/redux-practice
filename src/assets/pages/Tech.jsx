248201dd943745e77d7c78877459305fd6f76461
AdityaNair07 <nairaditya07@gmail.com>
2025-02-22 14:36:01 +0530

    ui fixes and added react icons

--- src/assets/pages/Tech.jsx
+++ src/assets/pages/Tech.jsx
@@ -1,10 +1,13 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Navbar from "../components/Navbar";
 import { addProduct, removeProduct } from "../redux/slice";
 
 import { ToastContainer, toast } from "react-toastify";
 import { fetchProducts } from "../api/fetchProducts";
 import { useEffect, useState } from "react";
+import { CgAdd } from "react-icons/cg";
+import { GrAdd } from "react-icons/gr";
+import { BiTrash } from "react-icons/bi";
 
 const successMsg = () => {
   toast.success("Item added to cart!", {
@@ -32,6 +35,10 @@ const Tech = () => {
     fetchProducts().then((data) => setProducts(data));
   }, []);
 
+  const cartItems = useSelector((state) => state.cart.items);
+
+  // console.log(cartItems, "------");
+
   return (
     <div className="lg:px-10 mx-auto relative py-20 bg-red-400 min-h-screen h-full flex flex-col gap-10">
       <Navbar />
@@ -39,28 +46,42 @@ const Tech = () => {
         {products?.map((item, index) => (
           <div
             key={index}
-            className="flex flex-col gap-4 border rounded-md shadow-md min-h-40 bg-neutral-300 p-4"
+            className="flex flex-col gap-4 border rounded-md shadow-md min-h-40 bg-neutral-300 p-4 relative"
           >
+            {cartItems.find((cartItem) => {
+              return cartItem.name == item.title;
+            }) && (
+              <div className="rounded-full shadow-md border cursor-pointer px-2 py-0 w-max bg-red-500 text-white border-transparent flex items-center justify-center absolute top-5 right-10">
+                {
+                  cartItems.filter((cartItem) => cartItem.name == item.title)
+                    .length
+                }
+              </div>
+            )}
             {item.title}
             <div className="flex items-center gap-4 mt-auto">
               <button
-                className="rounded-md shadow-md border cursor-pointer p-2 w-full bg-green-500"
+                className="rounded-md shadow-md border cursor-pointer p-2 w-full bg-green-500 text-white border-transparent flex items-center justify-center gap-2"
                 onClick={() => {
                   dispatch(addProduct({ name: item.title, price: item.price }));
                   successMsg();
                 }}
               >
-                Add Item
+                Add <GrAdd />
               </button>
-              <button
-                className="rounded-md shadow-md border cursor-pointer p-2 w-full bg-red-500"
-                onClick={() => {
-                  dispatch(removeProduct(item.title));
-                  removeMsg();
-                }}
-              >
-                Remove Item
-              </button>
+              {cartItems.find((cartItem) => {
+                return cartItem.name == item.title;
+              }) && (
+                <button
+                  className="rounded-md shadow-md border cursor-pointer p-2 w-full bg-red-500 text-white border-transparent flex items-center justify-center gap-2"
+                  onClick={() => {
+                    dispatch(removeProduct(item.title));
+                    removeMsg();
+                  }}
+                >
+                  Remove <BiTrash />
+                </button>
+              )}
             </div>
           </div>
         ))}
